def5160522e98635e88bd84d1778a4edd6e2286c
matibbaez <matiascaneda3@gmail.com>
2026-02-10 12:46:08 -0300

    Cambio en environment prod

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,10 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://reclamos-back-plantilla.onrender.com' 
+  apiUrl: 'https://estudio-api-yq5z.onrender.com', // Tu URL de Render
+
+  // 🎨 CONFIGURACIÓN MARCA BLANCA (WHITELABEL) - ¡ESTO FALTABA!
+  branding: {
+    nombre: 'Estudio Jurídico Digital',  
+    tituloWeb: 'Soluciones Legales Ágiles'
+  }
 };
